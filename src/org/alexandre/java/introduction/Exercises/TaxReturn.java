29a9b0f116bcc60007ed92995857820c6faa3bb4
Alexandre-Milim <alexandremilim15@gmail.com>
2025-05-03 23:19:08 -0300

    refactor: improve variable names in TaxReturn class

--- src/org/alexandre/java/introduction/Exercises/TaxReturn.java
+++ src/org/alexandre/java/introduction/Exercises/TaxReturn.java
@@ -2,18 +2,34 @@ package org.alexandre.java.introduction.Exercises;
 
 public class TaxReturn {
     public static void main(String[] args) {
-        double salary = 1518.50; 
-        
-        double tax = 0;
-        
-        if (salary >= 4664.00) {
-            tax = salary * 0.275;
-        } else if (salary >= 2259.00) {
-            tax = salary * 0.15;
+        double salary = 2500;
+        double oneTax = 7.5 / 100;
+        double secondeTax = 15 / 100;
+        double thirdTax = 22.5 / 100;
+        double fourthTax = 27.5 / 100;
+        double salarioComImposto;
+        double imposto;
+
+        if (salary <= 2428.80 ){
+            System.out.println("O salário de: "+ salary + " esta livre de imposto!");
+        } else if (salary >= 2428.80 && salary <= 2826.65){
+            imposto = salary * oneTax;
+            salarioComImposto = salary - imposto;
+            System.out.println(
+                "O valor de "+ "R$ "+imposto+" que corresponde a "+oneTax * 100 + "%" +" foi descontando de seu salário. " +
+                "O valor líquido do seu salário é: " + "R$ "+ salarioComImposto
+            );
+
+
+        }else if (salary >= 2826.66 && salary <= 3751.05){
+            salary = salary * secondeTax;
+            System.out.println("Seu novo salario com os impostos é: "+salary); 
+        }else if (salary >= 3751.06 && salary <= 4664.68){
+            salary = salary * thirdTax;
+            System.out.println("Seu novo sálario com os impostos é: "+salary);
+        } else {
+            salary = salary * fourthTax;
+            System.out.println("Seu novo salario com os impostos é: "+salary);
         }
-        
-        salary -= tax;
-        
-        System.out.println("Seu salário líquido é: " + salary);
     }
 }
