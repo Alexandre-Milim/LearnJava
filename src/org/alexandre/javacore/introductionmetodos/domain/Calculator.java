76f7465fe6a2c988eb44d9ea1443ee07a19e225b
Alexandre-Milim <alexandremilim15@gmail.com>
2025-07-02 23:03:11 -0300

    introduction methods

---
+++ src/org/alexandre/javacore/introductionmetodos/domain/Calculator.java
@@ -0,0 +1,7 @@
+package org.alexandre.javacore.introductionmetodos.domain;
+
+public class Calculator {
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+}
---
+++ src/org/alexandre/javacore/introductionmetodos/test/CalculatorTest.java
@@ -0,0 +1,10 @@
+package org.alexandre.javacore.introductionmetodos.test;
+
+import org.alexandre.javacore.introductionmetodos.domain.Calculator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.somaDoisNumeros();
+    }
+}
