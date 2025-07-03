8af4d5f3b650da6b2569cf7c9195bb4dcc0b4df7
Alexandre-Milim <alexandremilim15@gmail.com>
2025-07-02 22:32:51 -0300

    Correction of variables

--- src/org/alexandre/javacore/introductionclass/exercises/OOPCars.java
+++ src/org/alexandre/javacore/introductionclass/exercises/OOPCars.java
@@ -4,27 +4,27 @@ import org.alexandre.javacore.introductionclass.domain.exercises.Cars;
 
 public class OOPCars {
     public static void main(String[] args) {
-        Cars cars = new Cars();
-        Cars cars2 = new Cars();
+        Cars car1 = new Cars();
+        Cars car2 = new Cars();
 
-        cars.name = "296 GTB";
-        cars.model = "Ferrari";
-        cars.year = 2022;
+        car1.name = "296 GTB";
+        car1.model = "Ferrari";
+        car1.year = 2022;
 
-        cars2.name = "765LT Spider";
-        cars2.model = "Mclaren";
-        cars2.year = 2021;
+        car2.name = "765LT Spider";
+        car2.model = "Mclaren";
+        car2.year = 2021;
 
         System.out.println("=== First Car ===");
-        System.out.println("Name: " + cars.name);
-        System.out.println("Model: " + cars.model);
-        System.out.println("Year: " + cars.year);
+        System.out.println("Name: " + car1.name);
+        System.out.println("Model: " + car1.model);
+        System.out.println("Year: " + car1.year);
 
         System.out.println();
 
         System.out.println("=== Second Car ===");
-        System.out.println("Name: " + cars2.name);
-        System.out.println("Model: " + cars2.model);
-        System.out.println("Year: " + cars2.year);
+        System.out.println("Name: " + car2.name);
+        System.out.println("Model: " + car2.model);
+        System.out.println("Year: " + car2.year);
     }
 }
