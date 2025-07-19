d9ded7d6e811247a52a29bc4f148fca89959583a
alexandre <alexandremilim15@gmail.com>
2025-07-19 06:40:15 -0300

    Refactor Car class to enforce fixed speed limit and improve warning messages
    - Set speed limit as a final field with a default value of 250
    - Removed setter for speed limit to prevent external modification
    - Added warning message when speed limit is printed and equals or exceeds 250
    - Updated max speed setter to show alerts only when above 350
    - Made car name final to ensure immutability

--- src/org/alexandre/javacore/staticmodifiers/Test/CarTest.java
+++ src/org/alexandre/javacore/staticmodifiers/Test/CarTest.java
@@ -4,10 +4,9 @@ import org.alexandre.javacore.staticmodifiers.domain.Car;
 
 public class CarTest {
     public static void main(String[] args) {
-        Car car = new Car("Mustang", 300, 249);
+        Car car = new Car("Mustang", 300);
 
         car.setMaxSpeed(200);
-        car.setSpeedLimit(251);
         car.print();
     }
 }
--- src/org/alexandre/javacore/staticmodifiers/domain/Car.java
+++ src/org/alexandre/javacore/staticmodifiers/domain/Car.java
@@ -3,12 +3,11 @@ package org.alexandre.javacore.staticmodifiers.domain;
 public class Car {
     private String name;
     private double maxSpeed;
-    private double speedLimit;
+    private final double speedLimit = 250;
 
-    public Car(String name, double maxSpeed, double speedLimit){
+    public Car(String name, double maxSpeed){
         this.name = name;
         this.maxSpeed = maxSpeed;
-        this.speedLimit = speedLimit;
     }
 
     public void print() {
@@ -16,6 +15,10 @@ public class Car {
         System.out.println("Name: " + this.name);
         System.out.println("Max Speed: " + this.maxSpeed);
         System.out.println("Speed Limit: " + this.speedLimit);
+
+        if (this.speedLimit == 250) {
+            System.out.println("Speed limit is fixed at 250 and cannot be changed.");
+        }
     }
 
     public void setMaxSpeed(double maxSpeed) {
@@ -25,23 +28,9 @@ public class Car {
         } else if (maxSpeed >= 350) {
             System.out.println("Close to the speed limit, be careful!");
         }
-        // Se for menor que 350, nenhuma mensagem será exibida
     }
 
     public double getMaxSpeed() {
         return this.maxSpeed;
-    }
-
-    public void setSpeedLimit(double speedLimit) {
-        this.speedLimit = speedLimit;
-        if (speedLimit > 249) {
-            System.out.println("You are at the limit");
-        } else {
-            System.out.println("You are below the limit");
-        }
-    }
-
-    public double getSpeedLimit() {
-        return speedLimit;
     }
 }
