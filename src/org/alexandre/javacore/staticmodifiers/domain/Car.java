3e951642c72f332e7195fca7cc55ec9fce6f2375
alexandre <alexandremilim15@gmail.com>
2025-07-19 19:33:22 -0300

    Changed speedLimit from final to mutable and removed fixed warning message

--- src/org/alexandre/javacore/staticmodifiers/domain/Car.java
+++ src/org/alexandre/javacore/staticmodifiers/domain/Car.java
@@ -3,7 +3,7 @@ package org.alexandre.javacore.staticmodifiers.domain;
 public class Car {
     private String name;
     private double maxSpeed;
-    private final double speedLimit = 250;
+    private double speedLimit = 250;
 
     public Car(String name, double maxSpeed){
         this.name = name;
@@ -16,9 +16,6 @@ public class Car {
         System.out.println("Max Speed: " + this.maxSpeed);
         System.out.println("Speed Limit: " + this.speedLimit);
 
-        if (this.speedLimit == 250) {
-            System.out.println("Speed limit is fixed at 250 and cannot be changed.");
-        }
     }
 
     public void setMaxSpeed(double maxSpeed) {
